02b0d65f26d7ce098e339eebcbc2a9acda662131
Pranta <pranta@asl.aero>
2020-05-17 16:08:04 +0600

    favicon added in auth-main page

--- client/views/layouts/auth-main.php
+++ client/views/layouts/auth-main.php
@@ -16,7 +16,7 @@ AuthAssetBundle::register($this);
 <head>
     <meta charset="utf-8" />
     <title><?= Html::encode($this->title) ?></title>
-
+    <link rel="icon" href="images/logo/eVISA-logo-favicon.ico" sizes="48x48" >
     <meta name="description" content="Latest updates and statistic charts">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
