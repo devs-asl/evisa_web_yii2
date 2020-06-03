0478abae4cb4ceb0d84e4d739782815543c09051
pranta <pranta@asl.aero>
2020-06-03 15:23:17 +0600

    updated client main-local

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,35 +1,20 @@
 <?php
 return [
     'components' => [
-//        'db' => [
-//            'class' => 'yii\db\Connection',
-//            'dsn' => 'mysql:host=localhost;dbname=evisa',
-//            'username' => 'root',
-//            'password' => '',
-//            'charset' => 'utf8',
-//        ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=devs-db.cl0j9h0z9kwc.ap-southeast-1.rds.amazonaws.com;dbname=dev_evisa',
-            'username' => 'devs',
-            'password' => 'devsdb&2019',
+            'dsn' => 'mysql:host=192.168.30.16;dbname=evisa_dev',
+            'username' => 'pranta',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.mailtrap.io',
-                'username' => '802fff7bccb8e4',
-                'password' => 'd4c90dc0c44e29',
-                'port' => '2525',
-                'encryption' => 'tls',
-            ],
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => false,
+            'useFileTransport' => true,
         ],
     ],
 ];
