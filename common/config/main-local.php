ce5b5151bd4976424eeb1e3cea13271f490e33df
Pranta <pranta@asl.aero>
2020-08-23 16:57:54 +0600

    update gitignore and main-local

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,13 +3,12 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.30.16;dbname=evisa_dev',
-            'username' => 'pranta',
-            'password' => 'root',
-//            'dsn' =>'mysql:host=devs-db.cl0j9h0z9kwc.ap-southeast-1.rds.amazonaws.com;dbname=dev_evisa',
-//            'username' => 'devs',
-//            'password' => 'devsdb&2019',
-
+//            'dsn' => 'mysql:host=192.168.30.16;dbname=evisa_dev',
+//            'username' => 'pranta',
+//            'password' => 'root',
+            'dsn' =>'mysql:host=devs-db.cl0j9h0z9kwc.ap-southeast-1.rds.amazonaws.com;dbname=dev_evisa',
+            'username' => 'devs',
+            'password' => 'devsdb&2019',
             'charset' => 'utf8',
         ],
         'mailer' => [
