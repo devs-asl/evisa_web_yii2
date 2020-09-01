61e14d78997aa51dfb2d101e9a22ab3b8ac2f0f6
Pranta <pranta@asl.aero>
2020-09-01 13:11:23 +0600

    new test test

---
+++ client/views/layouts/newTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pranta
+ * Date: 1/9/20
+ * Time: 1:10 PM
+ */
--- client/views/site/login.php
+++ client/views/site/login.php
@@ -10,7 +10,7 @@ use common\helpers\UrlHelpers;
 use common\widgets\Alert;
 use yii\helpers\Url;
 
-$this->title = 'E-visa Test | Bangladesh | Login';
+$this->title = 'E-Visa Test Test | Bangladesh | Login';
 
 ?>
 <!-- start new -->
