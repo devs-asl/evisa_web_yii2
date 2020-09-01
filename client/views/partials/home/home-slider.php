d96c94462cd0c68dd3043e6e773809016259786b
Pranta <pranta@asl.aero>
2020-09-01 13:55:10 +0600

    update slider

--- client/views/partials/home/home-slider.php
+++ client/views/partials/home/home-slider.php
@@ -368,73 +368,73 @@
 <!--                    </li>-->
 
                     <!-- SLIDE 06 -->
-                    <li data-index="rs-3" data-transition="fade">
-
-                        <!-- MAIN IMAGE -->
-                        <img src="images/header/slider-06.jpg" alt=""  width="1920" height="1280">
-
-                        <!-- LAYER NR. 1 -->
-                        <div class="tp-caption montserrat fweight-5 text-white tp-resizeme slider-text"
-                             data-x="['right','right','center','center']" data-hoffset="['40','50','0','0']"
-                             data-y="['top','top','top','top']" data-voffset="['280','200','100','110']"
-                             data-fontsize="['50','40','30','20']"
-                             data-lineheight="['100','100','100','50']"
-                             data-width="none"
-                             data-height="none"
-                             data-whitespace="nowrap"
-                             data-transform_idle="o:1;"
-                             data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"
-                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                             data-mask_in="x:0px;y:0px;"
-                             data-mask_out="x:inherit;y:inherit;"
-                             data-start="1000"
-                             data-splitin="none"
-                             data-splitout="none"
-                             data-responsive_offset="on"
-                             style="z-index: 7; white-space: nowrap;">Welcome to Bangladesh </div>
-
-                        <!-- LAYER NR. 2 -->
-                        <div class="tp-caption raleway white text-white tp-resizeme slider-text"
-                             data-x="['right','right','center','center']" data-hoffset="['40','50','0','0']"
-                             data-y="['top','top','top','top']" data-voffset="['350','260','150','150']"
-                             data-fontsize="['28','28','22','22']"
-                             data-lineheight="['100','100','100','50']"
-                             data-width="none"
-                             data-height="none"
-                             data-whitespace="nowrap"
-                             data-transform_idle="o:1;"
-                             data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"
-                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                             data-mask_in="x:0px;y:0px;"
-                             data-mask_out="x:inherit;y:inherit;"
-                             data-start="1500"
-                             data-splitin="none"
-                             data-splitout="none"
-                             data-responsive_offset="on"
-                             style="z-index: 7; white-space: nowrap;">Take a moment to see all our
-                        </div>
-
-                        <!-- LAYER NR. 3 -->
-                        <div class="tp-caption raleway white text-white tp-resizeme slider-text"
-                             data-x="['right','right','center','center']" data-hoffset="['40','50','0','0']"
-                             data-y="['top','top','top','top']" data-voffset="['400','320','240','200']"
-                             data-fontsize="['28','28','22','22']"
-                             data-lineheight="['70','70','70','50']"
-                             data-width="none"
-                             data-height="none"
-                             data-whitespace="nowrap"
-                             data-transform_idle="o:1;"
-                             data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"
-                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                             data-mask_in="x:0px;y:0px;"
-                             data-mask_out="x:inherit;y:inherit;"
-                             data-start="2000"
-                             data-splitin="none"
-                             data-splitout="none"
-                             data-responsive_offset="on"
-                             style="z-index: 7; white-space: nowrap;">amazing places! </div>
-
-                    </li>
+<!--                    <li data-index="rs-3" data-transition="fade">-->
+<!---->
+<!--                        <!-- MAIN IMAGE -->-->
+<!--                        <img src="images/header/slider-06.jpg" alt=""  width="1920" height="1280">-->
+<!---->
+<!--                        <!-- LAYER NR. 1 -->-->
+<!--                        <div class="tp-caption montserrat fweight-5 text-white tp-resizeme slider-text"-->
+<!--                             data-x="['right','right','center','center']" data-hoffset="['40','50','0','0']"-->
+<!--                             data-y="['top','top','top','top']" data-voffset="['280','200','100','110']"-->
+<!--                             data-fontsize="['50','40','30','20']"-->
+<!--                             data-lineheight="['100','100','100','50']"-->
+<!--                             data-width="none"-->
+<!--                             data-height="none"-->
+<!--                             data-whitespace="nowrap"-->
+<!--                             data-transform_idle="o:1;"-->
+<!--                             data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"-->
+<!--                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"-->
+<!--                             data-mask_in="x:0px;y:0px;"-->
+<!--                             data-mask_out="x:inherit;y:inherit;"-->
+<!--                             data-start="1000"-->
+<!--                             data-splitin="none"-->
+<!--                             data-splitout="none"-->
+<!--                             data-responsive_offset="on"-->
+<!--                             style="z-index: 7; white-space: nowrap;">Welcome to Bangladesh </div>-->
+<!---->
+<!--                        <!-- LAYER NR. 2 -->-->
+<!--                        <div class="tp-caption raleway white text-white tp-resizeme slider-text"-->
+<!--                             data-x="['right','right','center','center']" data-hoffset="['40','50','0','0']"-->
+<!--                             data-y="['top','top','top','top']" data-voffset="['350','260','150','150']"-->
+<!--                             data-fontsize="['28','28','22','22']"-->
+<!--                             data-lineheight="['100','100','100','50']"-->
+<!--                             data-width="none"-->
+<!--                             data-height="none"-->
+<!--                             data-whitespace="nowrap"-->
+<!--                             data-transform_idle="o:1;"-->
+<!--                             data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"-->
+<!--                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"-->
+<!--                             data-mask_in="x:0px;y:0px;"-->
+<!--                             data-mask_out="x:inherit;y:inherit;"-->
+<!--                             data-start="1500"-->
+<!--                             data-splitin="none"-->
+<!--                             data-splitout="none"-->
+<!--                             data-responsive_offset="on"-->
+<!--                             style="z-index: 7; white-space: nowrap;">Take a moment to see all our-->
+<!--                        </div>-->
+<!---->
+<!--                        <!-- LAYER NR. 3 -->-->
+<!--                        <div class="tp-caption raleway white text-white tp-resizeme slider-text"-->
+<!--                             data-x="['right','right','center','center']" data-hoffset="['40','50','0','0']"-->
+<!--                             data-y="['top','top','top','top']" data-voffset="['400','320','240','200']"-->
+<!--                             data-fontsize="['28','28','22','22']"-->
+<!--                             data-lineheight="['70','70','70','50']"-->
+<!--                             data-width="none"-->
+<!--                             data-height="none"-->
+<!--                             data-whitespace="nowrap"-->
+<!--                             data-transform_idle="o:1;"-->
+<!--                             data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:1.5;sY:1.5;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"-->
+<!--                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"-->
+<!--                             data-mask_in="x:0px;y:0px;"-->
+<!--                             data-mask_out="x:inherit;y:inherit;"-->
+<!--                             data-start="2000"-->
+<!--                             data-splitin="none"-->
+<!--                             data-splitout="none"-->
+<!--                             data-responsive_offset="on"-->
+<!--                             style="z-index: 7; white-space: nowrap;">amazing places! </div>-->
+<!---->
+<!--                    </li>-->
 
                     <!-- SLIDE 07 -->
 <!--                    <li data-index="rs-3" data-transition="fade">-->
